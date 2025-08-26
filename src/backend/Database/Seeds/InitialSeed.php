35813c563cda7baa656741dcae2ca84757cd1258
João Vinicius de Paula <joao.depaula@99x.io>
2025-08-26 14:18:24 -0300

    Update InitialSeed.php

--- src/backend/Database/Seeds/InitialSeed.php
+++ src/backend/Database/Seeds/InitialSeed.php
@@ -12,16 +12,16 @@ class InitialSeed extends Seeder
     $this->db->table('orgs')->insert([
       'id' => 1,
       'slug' => 'org',
-      "domain" => "https://new-org.com",
-      'name' => 'Demo Organization',
+      "domain" => "http://localhost",
+      'name' => 'Organization',
     ]);
 
     $this->db->table('tenants')->insert([
       'id' => 1,
-      "domain" => "https://new-tenant.com",
+      "domain" => "http://tenant.com",
       'slug' => 'tenant',
-      "name" => "New Tenant",
-      "status" => "draft",
+      "name" => "Tenant",
+      "status" => "published",
       'config' => '{}',
       "org_id" => 1
     ]);
@@ -33,9 +33,7 @@ class InitialSeed extends Seeder
       "password" => password_hash("securepassword", PASSWORD_DEFAULT),
       "phone" => "+1234567890",
       "bio" => "User biography",
-      "email_verified" => false,
-      "org_id" => 1,
-      "tenant_id" => 1,
+      "email_verified" => true,
       "role" => ["Admin"]
     ]);
   }
